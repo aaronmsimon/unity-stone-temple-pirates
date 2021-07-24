b63d8eda5d9a53eeedaaaf337b033ef4c19bef9b
aaronmsimon <aaronsimon@gmail.com>
2021-07-24 10:22:42 -0700

    Separate Player from Ship Controller

--- Stone Temple Pirates/Assets/Scripts/ShipController.cs
+++ Stone Temple Pirates/Assets/Scripts/ShipController.cs
@@ -5,8 +5,6 @@ using UnityEngine.AI;
 
 public class ShipController : MonoBehaviour
 {
-    public Camera cam;
-
     NavMeshAgent ship;
 
     void Start()
@@ -14,22 +12,8 @@ public class ShipController : MonoBehaviour
         ship = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    public void Move(Vector3 location)
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-        //Physics.Raycast(ray, out hit);
-        //Debug.DrawLine(ray.origin, hit.point, Color.red);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                ship.SetDestination(hit.point);
-            }
-        }
+        ship.SetDestination(location);
     }
 }
